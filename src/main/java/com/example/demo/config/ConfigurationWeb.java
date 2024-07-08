91d64fe33c44e59e8e1bdd14c673a108d62d6aab
Willey Gonzaga <willey.oliveira12@gmail.com>
2024-07-08 00:28:00 -0300

    Atualizando arquivo SecurityConfig

--- src/main/java/com/example/demo/config/ConfigurationWeb.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.config;
-
-import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-public class ConfigurationWeb extends WebSecurityConfigurerAdapter {
-
-}
